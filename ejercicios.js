c343f8fa407897d9c800702854cdca1ee4f922e5
CamiloDev <camiloanla@gmail.com>
2023-09-29 19:38:31 -0500

    IMPLEMENTACIÓN
    -Se adiciono boton de calcular el área de un cuadrado.
    -Se agrega la logica para que muestre el resultado en un H2.
    -Se da tamaño al modal.

--- ejercicios.js
+++ ejercicios.js
@@ -1,14 +1,22 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const bntabrirmodal = document.querySelector("#bin-abrir-modal");
-    const bntcerrarmodal = document.querySelector("#bin-cerrar-modal");
+    const bntAbrirmodal = document.querySelector("#bin-abrir-modal");
+    const bntCerrarmodal = document.querySelector("#bin-cerrar-modal");
+    const bntCalcular = document.querySelector("#bin-CalArea-modal");
     const modal = document.querySelector("#modal");
 
-    bntabrirmodal .addEventListener("click", () => {
+    bntAbrirmodal .addEventListener("click", () => {
         modal.setAttribute("open", "true");
     });
 
-    bntcerrarmodal.addEventListener("click", () => {
-        modal.removeAttribute("open");
+    bntCalcular.addEventListener("click", ()=>{
+        var lado = parseInt(document.getElementById('lado').value);
+        var resultado = "El área de este cuadro es de: " + lado * lado;
+        document.getElementById('mostrar-resul').textContent = resultado
+    })
+
+    bntCerrarmodal.addEventListener("click", () => {
+       modal.removeAttribute("open");
+    
     });
 });
